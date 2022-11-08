557b771e3ad8c0559ace7686c42527bffc06feab
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-08 20:21:14 +0600

    Favicon Added

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <h2>Banner</h2>
+        <div className="card lg:card-side bg-base-100 shadow-xl mt-6 mb-8">
+            <figure><img src="https://placeimg.com/400/400/arch" alt="Album" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">New album is released!</h2>
+                <p>Click the button to listen on Spotiwhy app.</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Listen</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
